4ab81c1a9e23ea4df86bbc0f9970ba409d984745
CarlosHerAnd <37437891+CarlosHerAnd@users.noreply.github.com>
2018-04-12 14:03:13 +0200

    Add files via upload

---
+++ zProyectoFinEverisGrupalV0000/src/daosImpl/RespuestaspreguntasDAOImpl.java
@@ -0,0 +1,49 @@
+package daosImpl;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import modelo.Respuestaspreguntas;
+import daos.ConstantesSQL;
+import daos.GenericDAO;
+import daos.RespuestaspreguntasDAO;
+
+public  class RespuestaspreguntasDAOImpl extends GenericDAO implements RespuestaspreguntasDAO {
+
+	@Override
+	public List<Respuestaspreguntas> obtenerRespuestaspreguntas() {
+		conectar();
+		List<Respuestaspreguntas> respuestaspreguntas = new ArrayList<Respuestaspreguntas>();
+		try {
+			PreparedStatement ps = con.prepareStatement(ConstantesSQL.SELECCION_RESPUESTASPREGUNTAS);
+			ResultSet resultado = ps.executeQuery();
+			while(resultado.next()){
+				Respuestaspreguntas respuestapregunta = new Respuestaspreguntas();
+				respuestapregunta.setDescripcion(resultado.getString("descripcion"));
+				respuestaspreguntas.add(respuestapregunta);
+			
+			}//end while
+		} catch (SQLException e) {
+			System.out.println("SQL select respuestaspreguntas esta mal");
+			System.out.println(e.getMessage());
+		
+		}//end catch
+		desconectar();
+		
+		return respuestaspreguntas;
+	}
+
+
+
+	}
+
+
+
+
+
+	
+
+
