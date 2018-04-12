4ef1e057ca16c2e49952f4a780b5fc7150eac5d3
CarlosHerAnd <37437891+CarlosHerAnd@users.noreply.github.com>
2018-04-12 13:59:59 +0200

    Update ConstantesSQL.java

--- zProyectoFinEverisGrupalV0000/src/daos/ConstantesSQL.java
+++ zProyectoFinEverisGrupalV0000/src/daos/ConstantesSQL.java
@@ -2,10 +2,12 @@ package daos;
 
 public class ConstantesSQL {
 
-	public static final String IDENFITICAR_USUARIO = "select id from usuarios where usuario like ? and contraseña like ?";
+	public static final String IDENFITICAR_USUARIO = "select id from usuarios where usuario like ? and contraseÃ±a like ?";
 	public static final String IDENFITICAR_USUARIO_ADMIN = "select id from administradores where"
-			+ " usuario like ? and contraseña like ?";
-	public static final String REGISTRAR_USUARIO = "insert into usuarios (usuario,contraseña,nombre,apellidos,"
+			+ " usuario like ? and contraseÃ±a like ?";
+	public static final String REGISTRAR_USUARIO = "insert into usuarios (usuario,contraseÃ±a,nombre,apellidos,"
 			+ "fechaNacimiento,email,dni) values (?,?,?,?,?,?,?)";
+	public static final String SELECCION_RESPUESTASPREGUNTAS ="select * from respuestaspreguntas";
+	
 
 }
