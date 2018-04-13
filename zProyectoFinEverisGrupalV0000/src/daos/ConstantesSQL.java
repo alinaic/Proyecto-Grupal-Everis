65db21d7dd7829b377dd9aceca4d226e5bfc4aac
alveloso <alveloso@usal.es>
2018-04-13 14:33:50 +0200

    Update ConstantesSQL.java

--- zProyectoFinEverisGrupalV0000/src/daos/ConstantesSQL.java
+++ zProyectoFinEverisGrupalV0000/src/daos/ConstantesSQL.java
@@ -10,5 +10,9 @@ public class ConstantesSQL {
 	public static final String LISTAR_PREGUNTAS = "SELECT * FROM preguntas";
 	public static final String SELECCION_RESPUESTASPREGUNTAS ="select * from respuestaspreguntas";
 	
+	//Constantes para registrar respuestas de los usuarios
+	public static final String REGISTRAR_RESPUESTA_TEXTO = "insert into textrespuestas (respuesta, idpregunta) values (?,?)";
+	public static final String REGISTRAR_RESPUESTA_RADIO = "insert into radiorespuestas (respuesta, idpregunta) values (?,?)";
+	public static final String REGISTRAR_RESPUESTA_CHECK = "insert into checkrespuestas (respuestas, idpregunta) values (?,?)";
 
 }
