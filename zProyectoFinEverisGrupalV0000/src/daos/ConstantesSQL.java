a254304649d48d2ddeb6a43077a57f2e5a501df0
alveloso <alveloso@usal.es>
2018-04-14 20:20:00 +0200

    Update ConstantesSQL.java

--- zProyectoFinEverisGrupalV0000/src/daos/ConstantesSQL.java
+++ zProyectoFinEverisGrupalV0000/src/daos/ConstantesSQL.java
@@ -12,5 +12,10 @@ public class ConstantesSQL {
 	public static final String LISTAR_MUNICIPIOS = "SELECT * FROM municipios where provincia_id = ?";
 	public static final String LISTAR_PREGUNTAS = "SELECT * FROM preguntas";
 	public static final String SELECCION_RESPUESTASPREGUNTAS ="select * from respuestaspreguntas";
+	
+	//SQL para registrar respuestas del formulario
+	public static final String REGISTRAR_RESPUESTA_TEXTO = "insert into textrespuestas (respuesta, idpregunta) values (?,?)";
+	public static final String REGISTRAR_RESPUESTA_RADIO = "insert into radiorespuestas (respuesta, idpregunta) values (?,?)";
+	public static final String REGISTRAR_RESPUESTA_CHECK = "insert into checkrespuestas (respuestas, idpregunta) values (?,?)";
 
 }
